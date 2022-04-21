5ea7a8be61e6eaeb7ddb4d0eeaf378904fb9ac26
vsyrov <vsyrov@custis.ru>
2022-04-21 21:12:57 +0300

    feat(HomeWork1): корзина с количеством и резолвер

--- src/app/resolvers/product-card-full.resolver.ts
+++ src/app/resolvers/product-card-full.resolver.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
 import {
-  Router, Resolve,
+  Router,
+  Resolve,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
 } from '@angular/router';
-import {first, map, Observable, of, tap} from 'rxjs';
-import {IState} from "../store/reducers";
-import {select, Store} from "@ngrx/store";
-import {IProduct} from "../models/product-info.model";
-import {getProduct} from "../store/reducers/products.reducer";
+import { first, map, Observable, of, tap } from 'rxjs';
+import { IState } from '../store/reducers';
+import { select, Store } from '@ngrx/store';
+import { IProduct } from '../models/product-info.model';
+import { getProduct } from '../store/reducers/products.reducer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductCardFullResolver implements Resolve<IProduct | undefined> {
+  constructor(private store: Store<IState>) {}
 
-  constructor(private store: Store<IState>) { }
-
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<IProduct | undefined> {
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<IProduct | undefined> {
     const productId = route.paramMap.get('id') as string;
-    return this.store.pipe(
-      select(getProduct, productId),
-      first()
-    );
+    return this.store.pipe(select(getProduct, productId), first());
   }
 }
